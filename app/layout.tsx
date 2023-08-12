57b4fd92405ea16cae4c7e4cb85a7c7f9e751494
Pooya Molaei <pooyamollaei7@gmail.com>
2023-08-12 11:18:41 -0400

    components folder moved to root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Layout from "./components/layout/page";
+import Layout from "../components/layout/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import { useForm } from "react-hook-form";
+
+const SignUp = () => {
+    const form = useForm()
+    return(
+        <section></section>
+    )
+}
+export default SignUp;
